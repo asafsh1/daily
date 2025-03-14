1524be2c11a719eebecf7452f2b31eb5c9f84901
asaf shalev <asaf@MacBook-Air.local>
2025-03-14 13:43:02 +0700

    Update CORS configuration to allow requests from veleka-shipments-daily-report.netlify.app

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://vocal-cheesecake-1379ed.netlify.app', 'https://daily-shipment-tracker.netlify.app'],
+    origin: ['http://localhost:3000', 'https://vocal-cheesecake-1379ed.netlify.app', 'https://daily-shipment-tracker.netlify.app', 'https://veleka-shipments-daily-report.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
@@ -26,7 +26,7 @@ connectDB().then(connected => {
 // Initialize Middleware
 app.use(express.json({ extended: false }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://vocal-cheesecake-1379ed.netlify.app', 'https://daily-shipment-tracker.netlify.app'],
+  origin: ['http://localhost:3000', 'https://vocal-cheesecake-1379ed.netlify.app', 'https://daily-shipment-tracker.netlify.app', 'https://veleka-shipments-daily-report.netlify.app'],
   credentials: true
 }));
 
